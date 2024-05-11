9e4f4e9e472482ee4eec2e59bc80728e1e50adce
kateaeryn <kate.ae.christensen@gmail.com>
2024-05-11 10:59:15 -0700

    added elements to landing page

--- wdd430groupproject/app/ui/components/header.js
+++ wdd430groupproject/app/ui/components/header.js
@@ -1,19 +1,14 @@
 import React from "react";
 import logo from "/public/images/logo.png";
 import NavLinks from '/app/ui/dashboard/nav-links';
+import Search from '/app/ui/dashboard/search';
+import { UserCircleIcon, ShoppingCartIcon } from '@heroicons/react/24/outline';
 
 export default function Header() {
 	return (
-		<header style={{ backgroundColor: "#58592A" }}>
-			{/* Logo */}
-			<div
-				className="logo-container"
-				style={{
-					padding: "10px",
-					display: "flex",
-					justifyContent: "center",
-				}}
-			>
+		<header className="flex flex-col bg-tan">
+			
+			<div className="flex ">
 				<a href="/">
 					<img
 						src={logo.src}
@@ -21,37 +16,17 @@ export default function Header() {
 						style={{ width: "100px", height: "100px" }}
 					/>
 				</a>
+				<Search placeholder="Search the Haven..." />
+				<UserCircleIcon className=" w-[40px] h-[40px] m-4" />
+				<ShoppingCartIcon className="w-[40px] h-[40px] m-4"/>
 			</div>
 
 			{/* Navigation links */}
-			<div className="flex grow flex-row justify-between space-x-2 md:flex-row md:space-x-0 md:space-y-2">
+			<div className="flex grow flex-row justify-between space-x-2 md:flex-row md:space-x-0 md:space-y-2 ">
 			<NavLinks />
-			<div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
+			
 			</div>
-			{/* <nav
-				className="navigation"
-				style={{ padding: "20px", textAlign: "center", color: "#fff" }}
-			>
-				<ul
-                    className="nav-bar"
-					style={{
-						display: "flex",
-						justifyContent: "center",
-						alignItems: "center",
-						gap: "40px",
-					}}
-				>
-					<li className="nav-links">
-						<a href="/">Home</a>
-					</li>
-					<li className="nav-links">
-						<a href="/">Our Story</a>
-					</li>
-					<li className="nav-links">
-						<a href="/">Shop</a>
-					</li>
-				</ul>
-			</nav> */}
+			
 		</header>
 	);
 }
--- wdd430groupproject/app/ui/dashboard/nav-links.tsx
+++ wdd430groupproject/app/ui/dashboard/nav-links.tsx
@@ -5,12 +5,14 @@ import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
 
 const links = [
-    { name: 'Home', href: '/dashboard' },
+  { name: 'Home', href: '/' },
+  { name: 'Featured Items', href: '/dashboard' },
+  { name: 'Home Decor', href: '/dashboard/categories/decor' },
+  { name: 'Art', href: '/dashboard/categories/art' },
+  { name: 'Textiles', href: '/dashboard/categories/textiles' },
+    { name: 'Accessories', href: '/dashboard/categories/accessories' },
   {name: 'Our Story', href: '/dashboard/story'},
-  {
-    name: 'Shop',
-    href: '/dashboard/products',
-  },
+  
 ];
 
 export default function NavLinks() {
@@ -22,9 +24,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={clsx('flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+            className={clsx('flex  grow items-center justify-center align-bottom gap-2 p-3 text-2xl font-serif font-medium hover:bg-beige hover:text-darkBrown md:flex-none md:justify-start md:p-2 ',
               {
-                'bg-sky-100 text-blue-600': pathname === link.href,
+                'bg-beige text-darkBrown': pathname === link.href,
               },
             )}
             >
