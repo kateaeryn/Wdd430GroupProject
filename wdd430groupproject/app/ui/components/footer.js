f6b56532b85caea53331204d65db38afc69090b4
kateaeryn <kate.ae.christensen@gmail.com>
2024-05-14 20:33:55 -0700

    testing 4

--- wdd430groupproject/app/ui/components/footer.js
+++ wdd430groupproject/app/ui/components/footer.js
@@ -1,30 +1,4 @@
 import React from "react";
-<<<<<<< HEAD
-import logo from "/public/images/logo.png";
-import Image from "next/image";
-
-export default function Footer() {
-  return (
-    <footer
-      className="footer"
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-        backgroundColor: "#D9BCA3",
-        padding: "10px",
-      }}
-    >
-      {/* Logo */}
-      <a href="/">
-        <Image src={logo.src} alt="Logo" width={75} height={75} />
-      </a>
-
-      <p>&copy; 2024 Handcrafted Haven</p>
-    </footer>
-  );
-=======
 
 
 export default function Footer() {
@@ -37,5 +11,4 @@ export default function Footer() {
 			<p className="text-xl text-brown">&copy; 2024 Handcrafted Haven</p>
 		</footer>
 	);
->>>>>>> 6dec4fb4305712cafe0b5611fff3fdaaee19aa52
 }
