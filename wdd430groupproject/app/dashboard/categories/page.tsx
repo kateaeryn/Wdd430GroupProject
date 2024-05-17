9fdd787113984f4ec30d7cc4176bc691d394a3ab
kateaeryn <kate.ae.christensen@gmail.com>
2024-05-16 20:51:59 -0700

    tweaks

--- wdd430groupproject/app/dashboard/categories/page.tsx
+++ wdd430groupproject/app/dashboard/categories/page.tsx
@@ -1,20 +1,34 @@
 import { Metadata } from 'next';
 import ProductGrid from '@/app/ui/dashboard/products';
-
+import { Suspense } from "react";
+import { ProductsSkeleton } from "@/app/ui/skeletons";
+import { fetchFilteredItems } from '@/app/lib/data';
  
 export const metadata: Metadata = {
   title: 'Products',
 };
-
-export default function Page() {
+//category page needs to be keyed to take the category id or name
+//or something to filter by category, so which ever category is clicked
+//on the landing page brings up the right group of items
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || "";
+  const items = await fetchFilteredItems(query);
   return (
     <>
   <div className="flex flex-col  text-center space-y-10">
     <h1 className="text-brown text-6xl">Category ID</h1>
         <div className="flex flex-col md:flex-row md:flex-wrap md:justify-evenly lg:space-x-6 xl:space-x-12">
-        {/*The below comment line is preventing an error between typescript and async/await*/}
-        {/* @ts-expect-error Async Server Compnent*/}    
-          <ProductGrid />
+        
+           <Suspense key={query} fallback={<ProductsSkeleton />}>
+          <ProductGrid items={items} />
+          </Suspense>
         </div>    
       </div>
   
--- wdd430groupproject/app/ui/dashboard/products.tsx
+++ wdd430groupproject/app/ui/dashboard/products.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import clsx from "clsx";
-import blankBox from "/public/images/blank-box.png";
 import Image from "next/image";
 import { Item } from "@/app/lib/definitions";
 import { fetchItems } from "@/app/lib/data";
