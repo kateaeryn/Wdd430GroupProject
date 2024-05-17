9d8d80bbb66a9d85a03c643e9fab37a281eccc2e
marieiterer@gmail.com <marieiterer@gmail.com>
2024-05-16 19:27:20 -0600

    Shop page pull all products and search

--- wdd430groupproject/app/lib/definitions.ts
+++ wdd430groupproject/app/lib/definitions.ts
@@ -1,24 +1,35 @@
 export type User = {
-    id: string;
-    name: string;
-    email: string;
-    password: string;
+  id: string;
+  name: string;
+  email: string;
+  password: string;
 };
 
 export type Customer = {
-    id: string;
-    name: string;
-    email: string;
-    image_url: string;
+  id: string;
+  name: string;
+  email: string;
+  image_url: string;
 };
 
 export type Product = {
-    id: string,
-    artisan_id: string,
-    name: string,
-    price: number,
-    category: string,
-    description: string,
-    image_url: string,
-    status: string,
-}
+  id: string;
+  artisan_id: string;
+  name: string;
+  price: number;
+  category: string;
+  description: string;
+  image_url: string;
+  status: string;
+};
+
+export type Item = {
+  id: string;
+  artisan_id: string;
+  title: string;
+  price: number;
+  category: string;
+  description: string;
+  image_url: string;
+  status: string;
+};
--- wdd430groupproject/app/ui/dashboard/search.tsx
+++ wdd430groupproject/app/ui/dashboard/search.tsx
@@ -1,29 +1,36 @@
-'use client';
+"use client";
 
-
-// import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
 // import { useDebouncedCallback } from 'use-debounce';
 
-
 export default function Search({ placeholder }: { placeholder: string }) {
-  // const searchParams = useSearchParams();
-  // const pathname = usePathname();
-  // const { replace } = useRouter();
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
-//   const handleSearch = useDebouncedCallback((term) => {
-//       console.log(`Searching...${term}`);
-//     const params = new URLSearchParams(searchParams);
-//     params.set('page', '1');
-//       if (term) {
-//         params.set('query', term);
-//       } else {
-//         params.delete('query');
-//       }
-//       replace(`${pathname}?${params.toString()}`);
-    
-//     }, 300);
+  //   const handleSearch = useDebouncedCallback((term) => {
+  //       console.log(`Searching...${term}`);
+  //     const params = new URLSearchParams(searchParams);
+  //     params.set('page', '1');
+  //       if (term) {
+  //         params.set('query', term);
+  //       } else {
+  //         params.delete('query');
+  //       }
+  //       replace(`${pathname}?${params.toString()}`);
 
-  
+  //     }, 300);
+
+  function handleSearch(term: string) {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set("query", term);
+    } else {
+      params.delete("query");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }
+
   return (
     <div className="relative flex justify-center sm:h-60 sm:w-95 sm:justify-start sm:self-center ">
       <label htmlFor="search" className="sr-only">
@@ -32,12 +39,11 @@ export default function Search({ placeholder }: { placeholder: string }) {
       <input
         className="flex w-5/6 h-3/4 border bg-tan border-brown text-3xl outline-2 placeholder:text-brown  rounded-md p-2"
         placeholder={placeholder}
-        // onChange={(e) => {
-        //   handleSearch(e.target.value);
-        // }}
-        // defaultValue={searchParams.get('query')?.toString()}
+        onChange={(e) => {
+          handleSearch(e.target.value);
+        }}
+        defaultValue={searchParams.get("query")?.toString()}
       />
-      
     </div>
   );
 }
