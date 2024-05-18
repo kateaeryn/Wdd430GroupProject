5fb097d0292bb0967179c59e27976ca301b0f1f8
Daviles82 <avileshouston@live.com>
2024-05-18 15:33:46 -0500

    Registration for both customer and vendor

--- wdd430groupproject/app/dashboard/registration/page.tsx
+++ wdd430groupproject/app/dashboard/registration/page.tsx
@@ -1,74 +1,11 @@
-'use client';
-import React, { useState } from 'react';
+import React from 'react';
+import RegistrationForm from '@/app/ui/registration-form';
 
-export default function RegistrationForm() {
-  const [email, newEmail] = useState('');
-  const [password, newPassword] = useState('');
-  const [confirmPassword, newConfirmPassword] = useState('');
-  const [submit, newSubmit] = useState(false);
-  const [errors, setUpErrors] = useState<string[]>([]);
-
-  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    newSubmit(true);
-
-    if (password !== confirmPassword) {
-      setUpErrors(['Password & Confirm password do not match ']);
-      newSubmit(false);
-      return;
-    }
-
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    newEmail('');
-    newPassword('');
-    newConfirmPassword('');
-    newSubmit(false);
-  };
-
+export default function Page() {
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-y-2">
-      {errors.length > 0 && (
-        <ul>
-          {errors.map((error) => (
-            <li key={error} className="">
-              {error}
-            </li>
-          ))}
-        </ul>
-      )}
-      <input
-        value={email}
-        onChange={(e) => newEmail(e.target.value)}
-        type="email"
-        required
-        placeholder="Email"
-        className=""
-      />
-      <input
-        value={password}
-        onChange={(e) => newPassword(e.target.value)}
-        type="password"
-        required
-        placeholder="Password"
-        className=""
-      />
-      <input
-        value={confirmPassword}
-        onChange={(e) => newConfirmPassword(e.target.value)}
-        type="password"
-        required
-        placeholder="Confirm password"
-        className=""
-      />
-
-      <button
-        type="submit"
-        disabled={submit}
-        className="bg-red-800 disabled:bg-gray-800"
-      >
-        Submit
-      </button>
-    </form>
+    <div className="flex flex-col items-center">
+      <h2 className="mb-4">Registration</h2>
+      <RegistrationForm />
+    </div>
   );
 }
