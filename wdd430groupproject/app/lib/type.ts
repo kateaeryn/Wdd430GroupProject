e700ec00a5fcffcf2331205c47c5841f0a0891e1
Daviles82 <avileshouston@live.com>
2024-05-20 17:29:05 -0500

    registration image and data file update

--- wdd430groupproject/app/lib/type.ts
+++ wdd430groupproject/app/lib/type.ts
@@ -1,7 +1,5 @@
-import { sql } from '@vercel/postgres';
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
-import { redirect } from 'next/navigation';
 
 // Schema to validate registration
 export const registrationSchema = z
@@ -21,6 +19,7 @@ export const registrationSchema = z
       .trim()
       .regex(/^[a-zA-Z]+$/, 'Last name must contain only letters'),
     email: z.string().email(),
+    image: z.string().url(),
     password: z.string().min(5, 'Minimum 10 characters'),
     confirmPassword: z.string(),
   })
@@ -33,11 +32,12 @@ export const registrationSchema = z
 export type TRegistrationSchema = z.infer<typeof registrationSchema>;
 
 interface UserData {
-  registrationType: 'customer' | 'vendor';
   firstName: string;
   lastName: string;
   email: string;
+  image: string;
   password: string;
+  registrationType: 'customer' | 'vendor';
 }
 
 export async function processRegistration({
@@ -45,16 +45,27 @@ export async function processRegistration({
   firstName,
   lastName,
   email,
+  image,
   password,
 }: UserData) {
   try {
-    const insertQuery = `
-      INSERT INTO users (registrationType, firstName, lastName, email, password)
+    if (registrationType === 'customer') {
+      const insertQuery = `
+        INSERT INTO users (firstName, lastName, email, image, password)
+        VALUES (${1}, ${2}, ${3}, ${4}, ${5})
+        RETURNING *;
+      `;
+      const values = [firstName, lastName, email, image, password];
+      console.log(`Success registration at users, line 59 ${values}`);
+    } else if (registrationType === 'vendor') {
+      const insertQuery = `
+      INSERT INTO artisans (firstName, lastName, email, image, password)
       VALUES (${1}, ${2}, ${3}, ${4}, ${5})
       RETURNING *;
     `;
-    const values = [registrationType, firstName, lastName, email, password];
-    console.log(`Success registration, line 56 ${values}`);
+      const values = [firstName, lastName, email, image, password];
+      console.log(`Success registration at artisans, line 67 ${values}`);
+    }
   } catch (error) {
     console.error('Postgre insertion error:', error);
     return NextResponse.json({ error: 'Internal Server Error' });
